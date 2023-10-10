40907c29e869afb83eeeee80c173f42cbb19bae1
ManuelBasso <146716888+ManuelBasso@users.noreply.github.com>
2023-10-10 13:09:14 +0200

    upload exercise from jDoodle

---
+++ 2_SumChar.java
@@ -0,0 +1,12 @@
+//Dichiarare 2 caratteri e stampare come risultato la combinazione dei due caratteri affinché siano una stringa priva di spazi.
+
+
+public class SumChar {
+    public static void main(String args[]) {
+      char first = 'M';
+      char second = 'B';
+      String somma = "" + first + second;
+      
+      System.out.println("Sum Char 1st and 2nd = " + somma);
+    }
+}
